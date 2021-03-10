8f988d5e498afbfc14287a59c31e67dfae2f12cc
abdurahmanfz <zandt55@gmail.com>
2021-03-11 03:59:55 +0700

    clean

---
+++ node_modules/.cache/nuxt/vuetify/options.js
@@ -0,0 +1,1 @@
+export default {"theme":{"dark":false,"themes":{"dark":{"primary":"#1976d2","accent":"#424242","secondary":"#ff8f00","info":"#26a69a","warning":"#ffc107","error":"#dd2c00","success":"#00e676"},"light":{"primary":"#2a9d8f","accent":"#424242","secondary":"#ffdd00","info":"#26a69a","warning":"#F4A261","error":"#dd2c00","success":"#00e676"}}}}
